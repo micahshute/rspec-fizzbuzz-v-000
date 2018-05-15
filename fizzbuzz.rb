a5f46374f4945cf7de78e31a8d54bd7af29a2e96
Micah Shute <micah.shute@gmail.com>
2018-05-15 20:38:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,13 @@
-def fizzbuzz
+def fizzbuzz(num)
+  fizz = div_by_3(num) ? "Fizz" : ""
+  buzz = div_by_5(num) ? "Buzz" : ""
+  return fizz + buzz == "" ? nil : fizz + buzz
+end
 
+def div_by_3(num)
+  num % 3
+end
+
+def div_by_5(num)
+  num % 5
 end
